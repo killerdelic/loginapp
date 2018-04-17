bdb8c1e9ff555363ff459c07778d3a0033639644
Juan Carlos <juancarlos@MacBook-Pro-de-Juan.local>
2018-04-17 02:25:49 -0500

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCC2P357moVTJn9d9umimZv99ZW2x9csEk",
+    authDomain: "loginapp-932c7.firebaseapp.com",
+    databaseURL: "https://loginapp-932c7.firebaseio.com",
+    projectId: "loginapp-932c7",
+    storageBucket: "",
+    messagingSenderId: "240930699156"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCC2P357moVTJn9d9umimZv99ZW2x9csEk",
+    authDomain: "loginapp-932c7.firebaseapp.com",
+    databaseURL: "https://loginapp-932c7.firebaseio.com",
+    projectId: "loginapp-932c7",
+    storageBucket: "",
+    messagingSenderId: "240930699156"
+  }
 };
