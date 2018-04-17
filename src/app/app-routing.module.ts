223381b8e5f3137e0b277532ea42f312e2c1ab98
Juan Carlos <juancarlos@MacBook-Pro-de-Juan.local>
2018-04-17 01:09:16 -0500

    Maquetacion de LOGIN PAGE Y REGISTER PAGE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import {HomePageComponent} from './componentes/home-page/home-page.component';
 import {LoginPageComponent} from './componentes/login-page/login-page.component';
 import {RegisterPageComponent} from './componentes/register-page/register-page.component';
 import {PrivadoPageComponent} from './componentes/privado-page/privado-page.component';
-import { NotFoundPageComponent } from "./componentes/not-found-page/not-found-page.component";
+import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component';
 const routes: Routes = [
 
 {path: '', component: HomePageComponent},
-{path: 'login', component: HomePageComponent},
+{path: 'login', component: LoginPageComponent},
 {path: 'register',component: RegisterPageComponent},
 {path: 'privado', component: PrivadoPageComponent},
-{path: '**', component: NotFoundPageComponent}
+{path: '**', component: NotFoundPageComponent} 
 
 
 
